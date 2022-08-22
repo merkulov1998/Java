e4e5a868e537afca8e7e436e44733f037859bbe4
merkulov1998 <77933589+merkulov1998@users.noreply.github.com>
2022-08-22 20:52:10 +0400

    Maths

---
+++ Maths/src/Maths/Maths.java
@@ -0,0 +1,21 @@
+package Maths;
+
+import static java.lang.Math.PI;
+
+public class Maths {
+    public static void main(String[] args){
+        double sqroot= Math.sqrt(576);
+        double abs1= Math.abs(-1.23);
+        double pow1= Math.pow(3, 2);
+        double sqrt1= Math.sqrt(121.0) - Math.sqrt(256.0);
+        double abs_min1= Math.abs(Math.min(-3, -5));
+
+        System.out.println(PI);
+        System.out.println(sqroot);
+        System.out.println(abs1);
+        System.out.println(pow1);
+        System.out.println(sqrt1);
+        System.out.println(abs_min1);
+
+    }
+    }
